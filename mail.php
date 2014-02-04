d8d0cd72b66a64f242717c0b1e824c95cab7d1a1
Аоексей <AlexBaks@bk.ru>
2014-02-05 00:28:54 +0300

    перенесена руссофикация
    все теперь в шаблоне mail.tpl.php

--- mail.php
+++ mail.php
@@ -2,8 +2,8 @@
 require_once dirname(dirname(dirname(dirname(dirname(__FILE__))))).'/connectors/index.php';
 
 /*Проверка на то что этот пост содержит в себе способ оплаты*/
+<?php
 if (isset($_POST['pay'])) {
-    /*получаем привичный ключь FormSave*/
     $sql = "SHOW TABLE STATUS LIKE 'modx_formsave_forms'";
     $q = $modx->prepare($sql);
     $q->execute(array(0));
@@ -11,40 +11,78 @@ if (isset($_POST['pay'])) {
     
     foreach ($arr as $val) {
         $_POST['id'] =  $val['Auto_increment'];
-    }    
-}
-
-//------------------------------------------
-/*Русские синонимы*/
-$replace['phon'] = 'Телефон';
-$replace['imei'] = 'iMEi';	
-$replace['contry'] = 'Страна';	
-$replace['pay'] = 'Способ оплаты';
-$replace['info'] = 'Дополнительная информация';	
-$replace['id'] = 'Номер заказа';	
-
-/*Переписываем пост в буфер*/
-$buf = array();
-foreach ($_POST as $key => $val) {
-    if (!isset($replace[$key])) {
-       $buf[$key] = $val;
-    }else{
-        $buf[$replace[$key]] = $val;
+    }  
+    
+    if (empty($_POST['cost'])) {
+        $_POST['cost'] = $modx->resource->getTVValue('cost');
     }
+    $id = $_POST['id'];
+    $cost = $_POST['cost'];
 }
-unset($_POST);
-/*Русофицируем пост*/
-foreach ($buf as $key => $val) {
-    $_POST[$key] = $val;
-}
+$mail = $_POST['mail'];
 
 //------------------------------------------
-/*Отправка почты*/
+
 $modx->runSnippet('FormIt',array(
     'hooks'=>'FormSave,email'
-    ,'emailSubject'=>'Имя'
-    ,'emailTo'=>'52018@bk.ru'
+    ,'emailSubject'=>$modx->resource->longtitle
+    ,'emailTo'=> $modx->getOption('mail').','.$mail
     ,'emailFromName'=>'Maclab.ru'
+    ,'emailTpl'=>'mail.tpl'
 ));
+
+if (isset($id)) {
+    // 2.
+    // Оплата заданной суммы с выбором валюты на сайте ROBOKASSA
+    // Payment of the set sum with a choice of currency on site ROBOKASSA
+    
+    // регистрационная информация (логин, пароль #1)
+    // registration info (login, password #1)
+    $mrh_login = "korziner";
+    $mrh_pass1 = "123qwe123";
+    
+    // номер заказа
+    // number of order
+    $inv_id = $id;
+    
+    // описание заказа
+    // order description
+    $inv_desc = $modx->resource->longtitle;
+    
+    // сумма заказа
+    // sum of order
+    $out_summ = $cost;
+    
+    // тип товара
+    // code of goods
+    $shp_item = "2";
+    
+    // предлагаемая валюта платежа
+    // default payment e-currency
+    $in_curr = "";
+    
+    // язык
+    // language
+    $culture = "ru";
+    
+    // формирование подписи
+    // generate signature
+    $crc  = md5("$mrh_login:$out_summ:$inv_id:$mrh_pass1:Shp_item=$shp_item");
+    
+    // форма оплаты товара
+    // payment form
+    print "<html>".
+      "<form action='https://merchant.roboxchange.com/Index.aspx' method=POST>".
+      "<input type=hidden name=MrchLogin value=$mrh_login>".
+      "<input type=hidden name=OutSum value=$out_summ>".
+      "<input type=hidden name=InvId value=$inv_id>".
+      "<input type=hidden name=Desc value='$inv_desc'>".
+      "<input type=hidden name=SignatureValue value=$crc>".
+      "<input type=hidden name=Shp_item value='$shp_item'>".
+      "<input type=hidden name=IncCurrLabel value=$in_curr>".
+      "<input type=hidden name=Culture value=$culture>".
+      "<input class='btn btn-default see_more' type=submit value='Оплатить'>".
+      "</form></html>";
+}
 
 print '{"success":true,"result":"200","description":"OK"}';
