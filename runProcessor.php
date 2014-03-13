82fd8cb30c1cda08dde7852b2592acd75cc454e0
Аоексей <AlexBaks@bk.ru>
2014-03-13 20:16:12 +0300

    Update runProcessor.php

--- runProcessor.php
+++ runProcessor.php
@@ -4,3 +4,4 @@ $otherProps = array(
     'processors_path' => $modx->getOption('core_path') . 'components/modxsite/processors/'
 );
 $response = $modx->runProcessor('web/adverts/getdata', $processorProps, $otherProps);
+return $response->response;
